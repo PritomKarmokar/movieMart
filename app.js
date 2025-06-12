9905b14b8212d594aa6431d4c5aa17d38cb045bd
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-06-13 00:15:35 +0600

    feat: logger middleware

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express')
 const config = require("./config/setup")
 const logger = require("./config/logger")
 const connectDB = require("./config/db")
+const loggerMiddleware = require("./middleware/logger")
 
 const app = express()
 
 app.use(express.json())
 
+app.use(loggerMiddleware)
+
 connectDB()
 
 const PORT = config.port
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        msg: "Welcome, to this world"
+    })
+})
 
 app.listen(PORT, () => {
     logger.info(`Server running on port: ${PORT}`)
